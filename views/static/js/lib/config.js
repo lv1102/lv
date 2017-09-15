7e79d77b42d21985efbcc01cd85e2e0e834514a2
lv <lv@jh.com>
2017-09-15 20:05:31 +0800

    实现了commonjs模块的封装

--- views/static/js/lib/config.js
+++ views/static/js/lib/config.js
@@ -4,6 +4,7 @@ require.config({
     paths:{
         //如果要引入的模块是一个具名模块,那么这个别名一定要和具名模块的名称保持一致
         jquery : "./jquery/jquery",
-        cookie : "./jquery-cookie/jquery.cookie"
+        cookie : "./jquery-cookie/jquery.cookie",
+        template : "./artTemplate/template"
     }
 }); 
